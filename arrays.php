8809993af7aeb2f5865cf3c9a35a6ab1dd7b0ca2
alexngUNC <ageorgiev@unc.edu>
2022-08-06 15:33:54 -0400

    Finished array functions list

--- arrays.php
+++ arrays.php
@@ -11,6 +11,13 @@
     <h1>Array Functions</h1>
     <ul>
         <li><p class="arrFun">array_key_exists($key, $ar)</p> - Returns TRUE if key is set in the array</li>
+        <li><p class="arrFun">isset($ar]'key'])</p> - Returns TRUE if key is set in the array</li>
+        <li><p class="arrFun">count($ar)</p> - How many elements in the array</li>
+        <li><p class="arrFun">is_array($ar)</p> - Returns TRUE if a variable is in the array</li>
+        <li><p class="arrFun">sort($ar)</p> - Sorts the array values (loses key)</li>
+        <li><p class="arrFun">ksort($ar)</p> - Sorts the array by key</li>
+        <li><p class="arrFun">asort($ar)</p> - Sorts array by value, keeping key association</li>
+        <li><p class="arrFun">shuffle($ar)</p> - Shuffles the array into random order</li>
     </ul>
 </body>
 </html>
