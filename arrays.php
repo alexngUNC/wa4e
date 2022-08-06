52fdcc8d6fb4424b793e6e5943ab631d8abd23f6
alexngUNC <ageorgiev@unc.edu>
2022-08-06 14:07:15 -0400

    First commit

---
+++ arrays.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>arrays</title>
+    <link href="styles.css" rel="stylesheet" />
+</head>
+<body>
+    <h1>Array Functions</h1>
+    <ul>
+        <li><p class="arrFun">array_key_exists($key, $ar)</p> - Returns TRUE if key is set in the array</li>
+    </ul>
+</body>
+</html>
---
+++ loop.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="styles.css" rel="stylesheet" />
+</head>
+<body>
+    <h1>Loops</h1>
+    <pre>$stuff = array("name" => "Chuck", 
+                        "course" => "SI664"); 
+        foreach($stuff as $k => $v) { 
+            echo "Key=",$k," Val=",$v,"\n"; 
+        }
+    </pre>
+    <?php
+        $stuff = array("name" => "Chuck",
+                        "course" => "SI664");
+        foreach($stuff as $k => $v) {
+            echo "Key=",$k," Val=",$v,"<br />";
+        }
+    ?>
+
+    <h2>Counted Loop Through an Array</h2>
+    <pre>
+    $stuff = array("Chuck", "SI664");
+        for($i=0; $i < count($stuff); $i++) {
+            echo "I=",$i,"Val=",$stuff[$i],"\n";
+        }
+    </pre>
+    <?php
+        $stuff = array("Chuck", "SI664");
+        for($i=0; $i < count($stuff); $i++) {
+            echo "I=",$i," Val=",$stuff[$i],"<br />";
+        }
+    ?> 
+</body>
+</html>
