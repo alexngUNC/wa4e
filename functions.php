db79e211a29f8368780ffe8c4a6b31c58222d190
alexngUNC <ageorgiev@unc.edu>
2022-08-07 12:59:48 -0400

    8/7

--- functions.php
+++ functions.php
@@ -12,8 +12,11 @@
         <li>Do not start with $</li>
         <li>Start with letter or underscore - consist of letters, numbers, and underscores (_)</li>
         <li>Case does <em>not</em> matter (do <strong>NOT</strong> take advantage of this)</li>
+        <li>Call by value (the variable within the function is an "alias" to the actual variable, 
+            which is a copy of the actual variable in the function call) - the passed in variable isn't changed
+        <li>Call by reference with & (change argument)</li>
     </ul>
-    <h2>Greet function</h2>
+    <h3>Greet function</h3>
         <pre>
         function greet() {
             print "Hello\n";
@@ -27,6 +30,25 @@
         }
 
         greet();
+        ?>
+    <h3>Optional arguments</h3>
+        <pre>
+            function howdy($lang='en') {
+                if ( $lang == 'en') return "Howdy!";
+                if ( $lang == 'rus') return "Привет!";
+            }
+
+            echo howdy();
+            echo howdy('rus');
+        </pre>
+        <?php
+        function howdy($lang='en') {
+            if ( $lang == 'en') return "Howdy!";
+            if ( $lang == 'rus') return "Привет!";
+        }
+        
+        echo howdy().'<br />';
+        echo howdy('rus');
         ?>
 </body>
 </html>
