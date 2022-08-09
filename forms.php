704393d8460165ed0816e279867fad9694b54b1b
alexngUNC <ageorgiev@unc.edu>
2022-08-09 10:31:15 -0400

    Added forms to git

---
+++ forms.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>forms</title>
+</head>
+<body>
+    <h1>Forms in PHP</h1>
+
+    <h3>User input</h3>
+    <p>Forms are created with input HTML tags.</p>
+    <p>Parameters:</p>
+        <ul>
+            <li>type - type of input/action (e.g. text, submit, checkbox)</li>
+            <li>name - part of key-value pair for the server</li>
+            <li>id - for CSS styling</li>
+        </ul>
+        <form>
+            <p><label for="guess"><Input Guess</label>
+            <input type="text" name="guess" id="guess" /></p>
+            <input type="submit" />
+        </form>
+
+</body>
+</html>
