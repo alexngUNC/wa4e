5d6a231216781c2a0b8523b016d7b818284fb03d
alexngUNC <ageorgiev@unc.edu>
2022-08-10 14:02:02 -0400

    Added the tables page

---
+++ tables.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Relational Databases</h1>
+    <p>Relational databases model data by storing rows and columns in tables. The power of the
+        relational database lies in its ability to efficiently retrieve data from those tables
+        and in particular where there are multiple tables and the relationships between those
+        tables involved in the query.
+    <h3>Terminology</h3>
+    <ul>
+        <li>Database - contains many tables</li>
+        <li>relation (or table) - contains tuples and attributes</li>
+        <li>Tuple (or row) - a set of fields that generally represent an "object" 
+            like a person or a music track</li>
+        <li>Attribute (also a column or field) - one of possibly many elements of data corresponding
+            to the object represented by the row</li>
+    </ul>
+</body>
+</html>
